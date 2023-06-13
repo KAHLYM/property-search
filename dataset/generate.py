e22f7df136abb78b77a68b86c711eb4a7828e840
KAHLYM <wrightci01w@gmail.com>
2023-06-13 18:01:05 +0100

    #1 Refactor dataset. Add generate.py as entry point

---
+++ dataset/generate.py
@@ -0,0 +1,60 @@
+import argparse
+import logging
+import os
+import pathlib
+
+from core.dataset import DataSet
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from source.im_feeling_lucky import ImFeelingLucky
+from source.rightmove import Rightmove
+from webdriver_manager.chrome import ChromeDriverManager
+
+OUTPUT_DIRECTORY = os.path.join(str(pathlib.Path(__file__).parent.absolute()), "output")
+pathlib.Path(OUTPUT_DIRECTORY).mkdir(parents=True, exist_ok=True)
+
+# fmt: off
+# Arguments
+parser = argparse.ArgumentParser(description="Crawl website and download sub pages")
+parser.add_argument("--download", metavar="D", type=int, help="Number of pages to download", required=True)
+parser.add_argument("--log-level", metavar="L", type=int, help="Log level defined by python logging", required=False, default=20)
+parser.add_argument("--output", metavar="O", type=str, help="Output directory", required=False, default=OUTPUT_DIRECTORY)
+parser.add_argument("--wait", metavar="W", type=str, help="Time in seconds to wait for network", required=False, default=5)
+subparsers = parser.add_subparsers(help="Sources", dest="command")
+
+parser_rightmove = subparsers.add_parser("rightmove", help="Crawl rightmove and download sub pages")
+parser_rightmove.add_argument("--webpage", metavar="W", type=str, help="Initial webpage", required=True)
+parser_rightmove.add_argument("--next", metavar="N", type=str, help="X-path to next webpage", required=True)
+
+parser_im_feeling_lucky = subparsers.add_parser("ifl", help="I'm feeling lucky")
+
+args = parser.parse_args()
+# fmt: on
+
+# Logging
+logging_level = logging.DEBUG
+logger = logging.getLogger("main")
+logger.setLevel(logging_level)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging_level)
+formatter = logging.Formatter("%(asctime)s [%(levelname)-8s] %(message)s")
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+logger.setLevel(args.log_level)
+
+# Setup DataSet
+dataset = DataSet(args.output, logger)
+
+# Install Chrome webdriver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+wait = WebDriverWait(driver, args.wait)
+
+# Execute command
+if args.command == "rightmove":
+    Rightmove(args, dataset, driver, logger, wait)
+elif args.command == "ifl":
+    ImFeelingLucky(args, dataset, driver, logger, wait)
+
+# Teardown
+driver.quit()
---
+++ dataset/source/rightmove.py
@@ -0,0 +1,84 @@
+"""
+Script to crawl website and download sub pages
+python .\dataset\rightmove_navigator.py --webpage "https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E87490&sortType=6&propertyTypes=&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=" --next //*[@id="l-container"]/div[3]/div/div/div/div[3]/button --max-download 50
+"""
+import argparse
+import os
+import pathlib
+import queue
+import re
+import sys
+from argparse import Namespace
+from logging import Logger
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class Rightmove:
+    def __init__(
+        self,
+        args: Namespace,
+        dataset,
+        driver: WebDriver,
+        logger: Logger,
+        wait: WebDriverWait,
+    ):
+        self._args = args
+        self._dataset = dataset
+        self._driver = driver
+        self._logger = logger
+        self._wait = wait
+
+        self._execute()
+
+    def find_elements_and_put_in_queue(queue: queue.Queue, driver: WebDriver) -> None:
+        for element in driver.find_elements(
+            By.XPATH, '//*[@id="l-searchResults"]/div/div'
+        ):
+            queue.put(element)
+
+    def get_property_id(element: WebElement) -> str | None:
+        if regex_search_result := re.search(
+            "property-([0-9]*)", element.get_attribute("id")
+        ):
+            return regex_search_result.group(1)
+        return None
+
+    def subpage(property_id: str) -> str:
+        return f"https://www.rightmove.co.uk/properties/{ property_id }"
+
+    def _execute(self):
+        elements = queue.Queue()
+        self._find_elements_and_put_in_queue(elements, self._driver)
+
+        # Get inital webpage
+        self._driver.get(self._args.webpage)
+        self._logger.debug(f"Loaded webpage { self._args.webpage }")
+
+        downloads = 0
+        while elements.qsize() and downloads < self._args.download:
+            if property_id := self._get_property_id(elements.get()):
+                self._dataset.add(self._subpage(property_id), " ".join(sys.argv))
+                downloads += 1
+                elements.task_done()
+
+            if not elements.qsize():
+                # Load next page if available
+                # N.b. find_element will raise NoSuchElementException if no element is found so use
+                # find_elements which will return an empty list if no elements are found
+                if next_page := self._driver.find_elements(By.XPATH, self._args.next):
+                    next_page[0].click()
+                    self._logger.info(
+                        f"Moved to next webpage { self._driver.current_url }"
+                    )
+                    self._wait.until(
+                        EC.presence_of_element_located((By.XPATH, self._args.next))
+                    )
+                    self._find_elements_and_put_in_queue(elements, self._driver)
+                else:
+                    self._logger.debug(f"No next page element found")
+                    break
